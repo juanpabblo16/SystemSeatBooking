ac335a38c5af1c0a1e273b0a5a0bad34c0dc38a8
carlost1504 <carlost1504@gmail.com>
2023-04-21 17:50:42 -0500

    add_data_Estructure

---
+++ src/structure/MyPriorityQueue.java
@@ -0,0 +1,105 @@
+package structure;
+
+import java.util.Comparator;
+
+public class MyPriorityQueue<E> {
+
+    private final Comparator<E> comparator;
+    private int size;
+    private E[] elements;
+
+    private static final int DEFAULT_CAPACITY = 16;
+
+    public MyPriorityQueue(Comparator<E> comparator) {
+        this.comparator = comparator;
+        this.elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyPriorityQueue(int capacity, Comparator<E> comparator) {
+        this.comparator = comparator;
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be positive");
+        }
+        this.elements = (E[]) new Object[capacity];
+    }
+
+    public boolean add(E element) {
+        if (element == null) {
+            throw new NullPointerException("Null elements not allowed");
+        }
+        if (size >= elements.length) {
+            resize();
+        }
+        elements[size++] = element;
+        upHeap(size - 1);
+        return true;
+    }
+
+    public E peek() {
+        if (size == 0) {
+            return null;
+        }
+        return elements[0];
+    }
+
+    public E poll() {
+        if (size == 0) {
+            return null;
+        }
+        E result = elements[0];
+        elements[0] = elements[--size];
+        elements[size] = null;
+        downHeap(0);
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void resize() {
+        int newCapacity = elements.length * 2;
+        E[] newElements = (E[]) new Object[newCapacity];
+        System.arraycopy(elements, 0, newElements, 0, size);
+        elements = newElements;
+    }
+
+    private void upHeap(int index) {
+        E element = elements[index];
+        while (index > 0) {
+            int parentIndex = (index - 1) >>> 1;
+            E parent = elements[parentIndex];
+            if (comparator.compare(element, parent) >= 0) {
+                break;
+            }
+            elements[index] = parent;
+            index = parentIndex;
+        }
+        elements[index] = element;
+    }
+
+    private void downHeap(int index) {
+        E element = elements[index];
+        int half = size >>> 1;
+        while (index < half) {
+            int childIndex = (index << 1) + 1;
+            E child = elements[childIndex];
+            int rightChildIndex = childIndex + 1;
+            if (rightChildIndex < size && comparator.compare(child, elements[rightChildIndex]) > 0) {
+                childIndex = rightChildIndex;
+                child = elements[childIndex];
+            }
+            if (comparator.compare(element, child) <= 0) {
+                break;
+            }
+            elements[index] = child;
+            index = childIndex;
+        }
+        elements[index] = element;
+    }
+}
+
